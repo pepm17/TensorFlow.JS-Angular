74adc09b0ed785581f8ebd3e10c69fc632bd3fa0
Pablo Emilio <pablopupo1797@gmail.com>
2019-10-06 15:27:20 -0500

    desimportar socket io

--- TFJS-ObjectDetection/src/app/app.module.ts
+++ TFJS-ObjectDetection/src/app/app.module.ts
@@ -3,18 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SpeechSynthesisModule } from '@kamiazya/ngx-speech-synthesis';
 
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-
-
-const config: SocketIoConfig = { url: 'http://localhost:4000', options: {} };
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    SocketIoModule.forRoot(config),
     SpeechSynthesisModule.forRoot({
       lang: 'en',
       volume: 1.0,
